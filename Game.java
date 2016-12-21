7f79a7c6d9e7280a20948259f83dd37ff6967dda
Ahkeem Lang <langahkeem@gmail.com>
2016-12-21 10:16:15 -0600

    Made comments

--- Game.java
+++ Game.java
@@ -1,16 +1,21 @@
 public class Game {
+  // Declared here...
   private String mAnswer;
   private String mHits;
   pritvate String mMisses;
   /* Forcing the creation of the game to provide an answer
   so everytime it's created it provides an answer. */
   public Game(String answer) {
+    // Initialized here...
     mAnswer = answer;
     mHits = "";
     mMisses = "";
   }
+  /* Adding a method that will allow the user to
+  apply a guess towards the puzzle */
   public boolean applyGuess(char letter) {
     boolean isHit = mAnswer.indexOf(letter) >= 0;
+    // Updating our state...
     if (isHit) {
       mHits += letter;
     } else {
