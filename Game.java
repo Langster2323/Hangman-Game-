b76dd190cbed9eda6b40751f8e68f77e3029d7f5
Ahkeem Lang <langahkeem@gmail.com>
2016-12-21 10:09:02 -0600

    Declared and initialized methods

--- Game.java
+++ Game.java
@@ -1,8 +1,12 @@
 public class Game {
   private String mAnswer;
+  private String mHits;
+  pritvate String mMisses;
   /* Forcing the creation of the game to provide an answer
   so everytime it's created it provides an answer. */
   public Game(String answer) {
     mAnswer = answer;
+    mHits = "";
+    mMisses = "";
   }
 }
