7eb8024387f897564ef491459f8ef413a279e7bc
Ahkeem Lang <langahkeem@gmail.com>
2016-12-22 09:57:21 -0600

    Created another constructor. This method will allow game logic to be recongnized within the propter file.

--- Prompter.java
+++ Prompter.java
@@ -1,5 +1,9 @@
 /* This prompter object will do all of the IO or
 input and output */
 public class Prompter {
-
+  private Game mGame;
+  // Constructor...
+  public Prompter(Game game) {
+    mGame = game;
+  }
 }
