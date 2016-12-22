bc6aa071e0e0d3a6fe34d4fd3640ef2554f2072d
Ahkeem Lang <langahkeem@gmail.com>
2016-12-22 10:02:01 -0600

    prompted the user to make a guess

--- Prompter.java
+++ Prompter.java
@@ -6,4 +6,10 @@ public class Prompter {
   public Prompter(Game game) {
     mGame = game;
   }
+  public boolean promptForGuess() {
+    Console console = System.console();
+    String guessAsString = console.readLine("Enter a letter:  ");
+    char guess = guessAsString.charAt(0);
+    return mGame.applyGuess(guess);
+  }
 }
