c5b7bda885bf0f2a9b0f0772f5d112bf01aee0da
Ahkeem Lang <langahkeem@gmail.com>
2017-01-04 12:09:08 -0600

    finishing up a story

--- Game.java
+++ Game.java
@@ -8,12 +8,23 @@ class Game {
   //Forcing the creation of the game by creating a constructor
   public Game(String answer) {
     //Now these variable names are initialized...
-    this.answer = answer;
+    this.answer = answer.toLowerCase();
     hits = "";
     misses = "";
   }
+  //To group common functionality...
+  private char normalizeGuess(char letter) {
+    if (! Character.isLetter(letter)) {
+      throw new IllegalArgumentException("A letter is required");
+    }
+    letter = Character.toLowerCase(letter);
+     if (misses.indexOf(letter) != -1 || hits.indexOf(letter) != -1) {
+      throw new IllegalArgumentException(letter + " has already been guessed");
+    }
+  }
 
   public boolean applyGuess(char letter) {
+   letter = normalizeGuess(letter);
     boolean isHit = answer.indexOf(letter) != -1;
     if (isHit) {
       hits += letter;
@@ -28,14 +39,14 @@ class Game {
   }
 
   public String getCurrentProgress() {
-  String progress = "";
-  for (char letter : answer.toCharArray()) {
-    char display = '-';
-    if (hits.indexOf(letter) != -1) {
-      display = letter;
+    String progress = "";
+    for (char letter : answer.toCharArray()) {
+      char display = '-';
+      if (hits.indexOf(letter) != -1) {
+        display = letter;
+      }
+      progress += display;
     }
-    progress += display;
+    return progress;
   }
-  return progress;
-}
 }
--- Prompter.java
+++ Prompter.java
@@ -10,15 +10,25 @@ class Prompter {
   }
 
   public boolean promptForGuess() {
-    Scanner scanner = new Scanner(System.in);
-    System.out.print("Enter a letter:  ");
-    String guessInput = scanner.nextLine();
-    char guess = guessInput.charAt(0);
-    return game.applyGuess(guess);
+        Scanner scanner = new Scanner(System.in);
+    boolean isHit = false;
+    boolean isAcceptable = false;
+
+    do {
+      System.out.print("Enter a letter:  ");
+      String guessInput = scanner.nextLine();
+      char guess = guessInput.charAt(0);
+      try {
+        isHit = game.applyGuess(guess);
+        isAcceptable = true;
+      } catch(IllegalArgumentException iae) {
+        System.out.printf("%s. Please try again. %n" iae.getMessage());
+      }
+    } while(! isAcceptable);
+    return isHit;
   }
+
   public void displayProgress() {
-    System.out.printf("You have %d tries left to solve: %s%n",
-    game.getRemainingTries(), 
-    game.getCurrentProgress());
+    System.out.printf("You have %d tries left to solve: %s%n", game.getRemainingTries(), game.getCurrentProgress());
   }
 }
