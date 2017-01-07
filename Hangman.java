4bce09280d37c8c1f35a5c89d433cdbc5ced5aad
Ahkeem Lang <langahkeem@gmail.com>
2017-01-07 12:35:37 -0600

    Misspelled treehouse

--- Hangman.java
+++ Hangman.java
@@ -3,7 +3,7 @@ public class Hangman {
 
   public static void main(String[] args) {
     // Your incredible code goes here...
-    Game game = new Game("treehousse");
+    Game game = new Game("treehouse");
     Prompter prompter = new Prompter(game);
     while (game.getRemainingTries() > 0) {
           prompter.displayProgress();
